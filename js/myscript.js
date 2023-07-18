75f48f5876ebdc286665cdd45b88d9fa2cc63aad
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-07-18 15:42:22 +0200

    add: card generate password

--- js/myscript.js
+++ js/myscript.js
@@ -13,5 +13,14 @@ const number21 = "21";
 // PASSWORD GENERATE
 const passwordGenerate = firstname + lastname + colorFavorite + number21;
 
+// PASSWORD CREATE
+const passwordCreate = `
+<p>Nome: ${firstname},</p> 
+<p>Cognome: ${lastname},</p>
+<p>Colore preferito: ${colorFavorite},</p>
+<p>Numero: ${number21}, </p>
+<p>Passwor Creata: <b>${passwordGenerate}</b></p>
+`;
+
 // PRINT PASSWORD GENERATE IN HTML WITH ID
-document.getElementById("Password").innerHTML = passwordGenerate;
+document.getElementById("Password").innerHTML = passwordCreate;
